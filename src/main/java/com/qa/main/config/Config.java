036b7aa880064acecc12751181b0fe6506de0417
Gie <gfortuna@wa.com>
2021-02-10 12:53:37 +0000

    Added package structure, config and resources

---
+++ src/main/java/com/qa/main/config/Config.java
@@ -0,0 +1,17 @@
+package com.qa.main.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class Config {
+	
+	@Bean
+	@Scope("prototype")
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
