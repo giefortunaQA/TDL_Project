d19227049a2ecc4d00eca9326831d6b75380ac44
Gie <gfortuna@wa.com>
2021-02-11 15:58:56 +0000

    TDL-7 #Added working update method for item

--- src/main/java/com/qa/main/controller/ItemController.java
+++ src/main/java/com/qa/main/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,11 @@ public class ItemController {
 	@GetMapping("/read/{id}")
 	public ResponseEntity<ItemDto> readById(@PathVariable Long id){
 		return ResponseEntity.ok(this.service.readById(id));
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<ItemDto> update(@PathVariable Long id,@RequestBody ItemDto itemDto){
+		return new ResponseEntity<>(this.service.update(itemDto, id),HttpStatus.ACCEPTED);
 	}
 
 }
--- src/main/java/com/qa/main/service/ItemService.java
+++ src/main/java/com/qa/main/service/ItemService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.qa.main.dto.ItemDto;
 import com.qa.main.persistence.domain.Item;
 import com.qa.main.persistence.repo.ItemRepo;
+import com.qa.main.utils.SpringBeanUtil;
 
 @Service
 public class ItemService {
@@ -27,18 +28,24 @@ public class ItemService {
 		this.repo = repo;
 		this.mapper = mapper;
 	}
-	
+
 	public ItemDto create(Item item) {
 		return this.mapToIDto(this.repo.save(item));
 	}
-	
-	
+
 	public List<ItemDto> readAll() {
 		return this.repo.findAll().stream().map(this::mapToIDto).collect(Collectors.toList());
 	}
-	
+
 	public ItemDto readById(Long id) {
 		return this.mapToIDto(this.repo.findById(id).orElseThrow());
 	}
-	
+
+	public ItemDto update(ItemDto itemDto, Long id) {
+		Item calledItem = this.repo.findById(id).orElseThrow();
+		calledItem.setName(itemDto.getName());
+		SpringBeanUtil.mergeNotNull(itemDto, calledItem);
+		return this.mapToIDto(this.repo.save(calledItem));
+	}
+
 }
