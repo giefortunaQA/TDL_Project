590deb95cd80accc58725eaf4600c5ef786f9345
Gie <gfortuna@wa.com>
2021-02-17 20:53:31 +0000

    TDL-14 #Updating this branch

--- src/main/java/com/qa/main/config/AppConfig.java
+++ src/main/java/com/qa/main/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class Config {
+public class AppConfig {
 	
 	@Bean
 	@Scope("prototype")
