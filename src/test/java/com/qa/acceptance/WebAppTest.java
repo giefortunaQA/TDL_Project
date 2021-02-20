793fda1727121c11fd1df017d2f72aabf7ac2020
Gie <gfortuna@wa.com>
2021-02-20 14:14:24 +0000

    TDL-26 #Written all acceptance test. Driver still void

--- src/test/java/com/qa/acceptance/WebAppTest.java
+++ src/test/java/com/qa/acceptance/WebAppTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.test.context.ActiveProfiles;
 import com.relevantcodes.extentreports.ExtentReports;
@@ -55,7 +57,7 @@ public class WebAppTest {
 		if (contains) {
 			test.log(LogStatus.PASS, "List created");
 		}else {
-			test.log(LogStatus.FAIL, "Found: "+ result+"instead.");
+			test.log(LogStatus.FAIL, "Found: "+ result+" instead.");
 		}
 		assertTrue(contains);
 	}
@@ -78,7 +80,7 @@ public class WebAppTest {
 		if (contains) {
 			test.log(LogStatus.PASS, "No lists yet since test");
 		}else {
-			test.log(LogStatus.FAIL, "Found: "+ result+"instead.");
+			test.log(LogStatus.FAIL, "Found: "+ result+" instead.");
 		}
 		
 		if (result.equals("No Lists.")) {
@@ -96,33 +98,157 @@ public class WebAppTest {
 		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
 		driver.get(WebAppTestSetup.URL);
 //		 WHEN
-		test.log(LogStatus.INFO, "When we click the refresh button");
+		test.log(LogStatus.INFO, "When we read the first list");
 		WebAppTestSetup setup=new WebAppTestSetup(driver);
-		setup.getReadAllBtn().click();
+		setup.readFirstListById();
 
 		// THEN
-		test.log(LogStatus.INFO, "Then - I should see all the lists in the database");
+		test.log(LogStatus.INFO, "Then - I should see this list and the items in it");
 		String result = setup.getDisplayDivRead().getText();
-		boolean contains = result.contains("Prepopulated List 1")&& result.contains("Prepopulated List 2");
+		boolean contains = result.contains("Prepopulated List 1");
 		if (contains) {
-			test.log(LogStatus.PASS, "All lists are displayed");
+			test.log(LogStatus.PASS, "List displayed successfully.");
+		}else {
+			test.log(LogStatus.FAIL, "Found: "+ result+" instead.");
+		}
+		assertTrue(contains);
+	}
+	
+	@Test
+	public void testUpdateList() throws Exception{
+		test=report.startTest("Update list by Id Test");
+//		 GIVEN
+		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
+		driver.get(WebAppTestSetup.URL);
+//		 WHEN
+		test.log(LogStatus.INFO, "When we read the first list");
+		WebAppTestSetup setup=new WebAppTestSetup(driver);
+		setup.readFirstListById();
+
+		test.log(LogStatus.INFO, "And when we click the edit/pencil button");
+		setup.update("Update");
+		// THEN
+		test.log(LogStatus.INFO, "Then - I should see this list with new name");
+		String result = setup.getDisplayDivRead().getText();
+		boolean contains = result.contains("Update");
+		if (contains) {
+			test.log(LogStatus.PASS, "List updated successfully.");
+		}else {
+			test.log(LogStatus.FAIL, "Found: "+ result+" instead.");
+		}
+		assertTrue(contains);
+	}
+	
+	@Test
+	public void testDeleteList() throws Exception{
+		test=report.startTest("Delete list Test");
+//		 GIVEN
+		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
+		driver.get(WebAppTestSetup.URL);
+//		 WHEN
+		test.log(LogStatus.INFO, "When we read the first list");
+		WebAppTestSetup setup=new WebAppTestSetup(driver);
+		setup.readFirstListById();
+
+		test.log(LogStatus.INFO, "And when we click the delete button");
+		setup.getDelBtn().click();
+		// THEN
+		test.log(LogStatus.INFO, "Then - I should see a confirmation of deletion");
+		String result = setup.getDisplayDivDelete().getText();
+		boolean contains = result.contains("To Do List deleted permanently.");
+		if (contains) {
+			test.log(LogStatus.PASS, "List delete successfully.");
+		}else {
+			test.log(LogStatus.FAIL, "Found: "+ result+"  instead.");
+		}
+		assertTrue(contains);
+	}
+	
+	@Test
+	public void testCreateItem() {
+		test=report.startTest("Create a item test");
+		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
+		driver.get(WebAppTestSetup.URL);
+//		 WHEN
+		test.log(LogStatus.INFO, "When we read the first list");
+		WebAppTestSetup setup=new WebAppTestSetup(driver);
+		setup.readFirstListById();
+		test.log(LogStatus.INFO, "And when I click the add task button");
+		setup.getAddTaskBtn1().click();
+		test.log(LogStatus.INFO, "And when I input values to the form");
+		setup.createItemInList1("Test Item", "no");
+		// THEN
+		test.log(LogStatus.INFO, "Then - I should see the item inside its parent list record");
+		String result = setup.getDisplayDivRead().getText();
+		boolean contains = result.contains("Test Item");
+		if (contains) {
+			test.log(LogStatus.PASS, "List created");
+		}else {
+			test.log(LogStatus.FAIL, "Found: "+ result+" instead.");
+		}
+		assertTrue(contains);
+	}
+	
+	@Test
+	public void testUpdateItem() {
+		test=report.startTest("Update Item Test");
+//		 GIVEN
+		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
+		driver.get(WebAppTestSetup.URL);
+//		 WHEN
+		test.log(LogStatus.INFO, "When we read the first list");
+		WebAppTestSetup setup=new WebAppTestSetup(driver);
+		setup.readFirstListById();
+
+		test.log(LogStatus.INFO, "And when we click the edit item button");
+		setup.getEditItem1Btn().click();
+		test.log(LogStatus.INFO, "And when we input updated details");
+		setup.updateItemInList1("Updated","yes");
+		// THEN
+		test.log(LogStatus.INFO, "Then - I should see this task with new name and marked done");
+		String resultBox = setup.getDisplayDivRead().getText();
+		WebElement resultItem=driver.findElement(By.tagName("strike"));
+		boolean checkUpdatedName=resultItem.getText().equals("Updated");
+		boolean checkUpdatedDone = resultItem.isDisplayed();
+		if (checkUpdatedDone) {
+			test.log(LogStatus.PASS, "Item marked done successfully.");
+		}else {
+			test.log(LogStatus.FAIL, "Item not striked.");
+		}
+		
+		if(checkUpdatedName) {
+			test.log(LogStatus.PASS, "Item name updated successfully");
+		}else {
+			test.log(LogStatus.FAIL, "Found: "+resultBox+" instead");
+		}
+		assertTrue(checkUpdatedDone);
+		assertTrue(checkUpdatedName);
+	}
+	
+	@Test
+	public void testDeleteItem() {
+		test=report.startTest("Delete Item Test");
+//		 GIVEN
+		test.log(LogStatus.INFO, "Given - we can access the To Do List webpage");
+		driver.get(WebAppTestSetup.URL);
+//		 WHEN
+		test.log(LogStatus.INFO, "When we read the first list");
+		WebAppTestSetup setup=new WebAppTestSetup(driver);
+		setup.readFirstListById();
+
+		test.log(LogStatus.INFO, "And when we click the delete item 1 button");
+		setup.getDeleteItem1Btn().click();
+		// THEN
+		test.log(LogStatus.INFO, "Then - I should see the update list without this item");
+		String result = setup.getDisplayDivRead().getText();
+		boolean success = !result.contains("Prepopulated Item 1");
+		if (success) {
+			test.log(LogStatus.PASS, "Item deleted successfully.");
 		}else {
 			test.log(LogStatus.FAIL, "Found: "+ result+"instead.");
 		}
-		
-		if (result.equals("No Lists.")) {
-			test.log(LogStatus.FAIL, "Element not prepopulated");
-		}else {
-			test.log(LogStatus.PASS, "Details loaded");
-		}
-		assertNotEquals("No Lists.", result);
-		assertTrue(contains);
+		assertTrue(success);
 	}
-//	@Test
-//	public void testUpdateList() throws Exception{
-//		
-//	}
-	
 	
 	@AfterEach
 	public void afterEach() {
--- src/test/java/com/qa/acceptance/WebAppTestSetup.java
+++ src/test/java/com/qa/acceptance/WebAppTestSetup.java
@@ -33,13 +33,73 @@ public class WebAppTestSetup {
 	
 	@FindBy(id="createListBtn")
 	private WebElement createListBtn;
+
 	
 	@FindBy(id="refreshBtn")
 	private WebElement refreshBtn;
-
+	
+	@FindBy(id="YourList1")
+	private WebElement yourList1Btn;
+	
+	@FindBy(id="Edit1")
+	private WebElement editBtn;
+	
+	@FindBy(id="nameUpdate")
+	private WebElement listNameUpdate;
+	
+	@FindBy(id="updateBtn")
+	private WebElement updateBtn;
+	
+	@FindBy(id="Delete1")
+	private WebElement delBtn;
+	
+	@FindBy(id="displayDivDelete")
+	private WebElement displayDivDelete;
+	@FindBy(id="createItemBtn")
+	private WebElement createItemBtn;
+	@FindBy(id="addTaskBtn1")
+	private WebElement addTaskBtn1;
+	@FindBy(id="itemName")
+	private WebElement itemName;
+	@FindBy(id="itemDone")
+	private WebElement itemDone;
+	@FindBy(id="editItem1")
+	private WebElement editItem1Btn;
+	@FindBy(id="itemNameUpdate")
+	private WebElement itemNameUpdate;
+	@FindBy(id="itemDoneUpdate")
+	private WebElement itemDoneUpdate;
+	@FindBy(id="updateItemBtn")
+	private WebElement updateItemBtn;
+	@FindBy(id="DeleteItem1")
+	private WebElement deleteItem1Btn;
+	
 	public void createList(String name) {
 		createFormBtn.click();
 		listName.sendKeys(name);
 		createListBtn.click();
 	}
+	
+	public void readFirstListById(){
+		refreshBtn.click();
+		yourList1Btn.click();
+	}
+	
+	public void update(String name) {
+		editBtn.click();
+		listNameUpdate.sendKeys(name);
+		updateBtn.click();
+	}
+	
+	public void createItemInList1(String name, String done) {
+		itemName.sendKeys(name);
+		itemDone.sendKeys(done);
+		createItemBtn.click();
+	}
+	public void updateItemInList1(String name,String done) {
+		itemNameUpdate.sendKeys(name);
+		itemDoneUpdate.sendKeys(done);
+		updateItemBtn.click();
+	}
+
 }
